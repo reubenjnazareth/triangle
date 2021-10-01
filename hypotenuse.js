cede481d9c987836e4559737d2e83afa79d8e0f3
rnaz25 <rnaz25@gmail.com>
2021-10-02 00:22:15 +0530

    hypo logic completed

--- hypotenuse.js
+++ hypotenuse.js
@@ -2,24 +2,33 @@ const sides = document.querySelectorAll(".side-input");
 const calculateBtn = document.querySelector("#calc-hyp-btn");
 const outputElement = document.querySelector("#outputHypotenuse");
 
+
+
 function calcSumOfsq(a, b) {
-    const sumOfsq = a*a + b*b;
+    const sumOfsq = a * a + b * b;
     //console.log(sumOfsq)
     return sumOfsq;
-} 
+}
 
 function calcHypotenuse() {
-    const sumOfsq = calcSumOfsq(Number(sides[0].value), Number(sides[1].value));
-    //console.log(sumOfsq);
-    const hypotenuseLength = Math.sqrt(sumOfsq);
-    //console.log(hypotenuseLength)
-    outputElement.innerText = "Hypotenuse Length is " + hypotenuseLength;
+    var a = Number(sides[0].value);
+    var b = Number(sides[1].value);
+    if (a < 0 || b < 0) {
+        outputElement.innerText = "Invalid entry. Enter values greater than 0 only"
+    } else {
+        const sumOfsq = calcSumOfsq(a, b);
+        //console.log(sumOfsq);
+        const hypotenuseLength = Math.sqrt(sumOfsq);
+        //console.log(hypotenuseLength)
+        outputElement.innerText = "Hypotenuse Length is " + hypotenuseLength + " units";
+    }
+}
 
-} 
+calculateBtn.addEventListener("click", calcHypotenuse)
+//Number(sides[0].value), Number(sides[1].value)
 
 
 
 
 
 calculateBtn.addEventListener("click", calcHypotenuse)
-//Number(sides[0].value), Number(sides[1].value)
